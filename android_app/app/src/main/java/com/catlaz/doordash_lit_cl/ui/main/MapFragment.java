d9c3cee4099ce4e3e8a020d6c4f096e35bb6c1d5
Cat <clazarom@hawk.iit.edu>
2021-01-30 09:17:40 -0800

    add map fragment

--- android_app/app/src/main/java/com/catlaz/doordash_lit_cl/ui/main/MapFragment.java
+++ android_app/app/src/main/java/com/catlaz/doordash_lit_cl/ui/main/MapFragment.java
@@ -12,20 +12,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
 /**
  * A placeholder fragment containing a simple map imageView.
  *
  * @author Caterina lazaro
  * @version 1.0 Jan 2021
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback  {
     private static final String _TAG = "MAP_FRAGMENT";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-       return inflater.inflate(R.layout.fragment_page_map, container, false);
+
+
+        return inflater.inflate(R.layout.fragment_page_map, container, false);
     }
 
     @Override
@@ -33,7 +40,17 @@ public class MapFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.d(_TAG, "View Created");
         //<TODO>
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
+    /* *********************************************
+        MAPS CALLBACKS
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(_TAG, "Map ready");
+    }
 }
